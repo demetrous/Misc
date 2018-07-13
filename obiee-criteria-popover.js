253c7a387aa08d2bae1e5932ea594ed75b416bb5
Dmitrii Karaulanov <demetrous@outlook.com>
2018-07-13 11:43:24 -0700

    works with dynamic elements

--- obiee-criteria-popover.js
+++ obiee-criteria-popover.js
@@ -4,20 +4,147 @@ window.onload = function () {
     if (originalOnload) {
         originalOnload();
     }
-    // $(document).ready(function () {
-    //     // $("[title='Census Term Selection']").popover({
-    //     //     title: "title",
-    //     //     content: 'conjkdfjdkf kdsfjgkdsjf s dfkgjs  dfkjgkdsjfgs dfjksdf gs jdfkjgdlskfjg dskfg  sdfkgj dfgtent',                
-    //     //     html: true,
-    //     //     placement: 'right'
-    //     // }).popover('show');
+    $(document).ready(function () {
 
-    //     console.log($("#criteriaDataBrowser"));
-    // });
+        //console.log($("#answerPickerDivId"));
+        //console.log(document.getElementById('criteriaDataBrowser'));
+        //$("#criteriaDataBrowser span span span.treeNodeText").click(console.log($(this)));
+
+        $(document).on("click", "#criteriaDataBrowser span span.treeNodeDetails span.treeNodeText", function () {
+            //$(this).attr('data-tippy','');
+            console.log($(this).text());
+
+            fetch('https://ir.unlv.edu/UmbracoODS/umbraco/api/DataDictionaryApi/GetJson').then(resp => resp.json()).then(data => {
+
+
+                var dataParsed = JSON.parse(data)
+                var contentHtml = ""
+
+                //console.log(content)
+
+                for (var key in dataParsed) {
+                    if (dataParsed.hasOwnProperty(key)) {
+
+                        if (key == "Name") {
+                            //continue;
+                        }
+
+                        contentHtml += "<div class=\"p-0 m-0 mb-2\"><h4 class=\"mb-0 pb-0\" style=\"font-size:1rem;\">" + key + "</h4>" + dataParsed[key] + "</div>";
+                    }
+                }
+
+                console.log(contentHtml)
+                //content.innerHTML = contentHtml//'<img width="500" height="500" src="' + url + '">'
+                //tip.loading = false
+            }).catch(e => {
+                console.log('Failed to load the data')
+                //tip.loading = false
+            })
+
+            $(this).attr("id", "newItem");
+
+
+
+            var newDiv = document.createElement("div");
+
+            newDiv.id = "newDiv";
+
+            //newDiv.style = "display: none;"
+
+            // and give it some content 
+            var newContent = document.createTextNode("Loading the content...");
+            // add the text node to the newly created div
+            newDiv.appendChild(newContent);
+
+            document.getElementById("criteriaDataBrowser").appendChild(newDiv);
+
+            const template = document.querySelector('#newDiv')
+            const initialText = template.textContent;
+
+            const tip = tippy('#newItem', {
+                html: document.querySelector('#newDiv'),
+                maxWidth: "100px",
+                placement: "right",
+                html: '#newDiv',
+                onShow() {
+                    // `this` inside callbacks refers to the popper element
+                    const content = this.querySelector('.tippy-content')
+
+                    console.log(this)
+
+                    if (tip.loading || content.innerHTML !== initialText) return
+
+                    tip.loading = true
+
+
+                    fetch('https://ir.unlv.edu/UmbracoODS/umbraco/api/DataDictionaryApi/GetJson').then(resp => resp.json()).then(data => {
+
+
+                        var dataParsed = JSON.parse(data)
+                        var contentHtml = ""
+
+                        //console.log(content)
+
+                        for (var key in dataParsed) {
+                            if (dataParsed.hasOwnProperty(key)) {
+
+                                if (key == "Name") {
+                                    //continue;
+                                }
+
+                                contentHtml += "<div class=\"p-0 m-0 mb-2\"><h4 class=\"mb-0 pb-0\" style=\"font-size:1rem;\">" + key + "</h4>" + dataParsed[key] + "</div>";
+                            }
+                        }
+
+
+                        content.innerHTML = contentHtml//'<img width="500" height="500" src="' + url + '">'
+                        tip.loading = false
+                    }).catch(e => {
+                        content.innerHTML = 'Failed to load a definition'
+                        tip.loading = false
+                    })
+                },
+                onHidden() {
+                    const content = this.querySelector('.tippy-content')
+                    content.innerHTML = initialText
+                }
+            })
+
+            // $(this).attr('title','Hi from tippy').addClass("myClass");
+            // tippy(".myClass");
+
+            // var newDiv = document.createElement("button");
+
+            // newDiv.className = "newBtn";
+            // newDiv.title = "Hello from button";
+
+            // //newDiv.style = "display: none;"
+
+            // // and give it some content 
+            // var newContent = document.createTextNode("Loading the content...");
+            // // add the text node to the newly created div
+            // newDiv.appendChild(newContent);
+
+            // // add the newly created element and its content into the DOM 
+            // document.getElementById("criteriaDataBrowser").appendChild(newDiv);
+            // tippy(".newBtn");
+
+            // var script = document.createElement("script");
+            // script.type = "text/javascript";
+            // script.src = 'tippy(".newBtn");';
+            // document.getElementsByTagName("#criteriaDataBrowser")[0].appendChild(script);
+
+
+
+        });
+
+    });
 };
 
 
-console.log('hi from criteria');
+//console.log('hi from criteria');
+
+
 
 
 function addElement() {
@@ -28,45 +155,67 @@ function addElement() {
     newDiv.style = "display: none;"
 
     // and give it some content 
-    var newContent = document.createTextNode("Loading a new image...");
+    var newContent = document.createTextNode("Loading the content...");
     // add the text node to the newly created div
     newDiv.appendChild(newContent);
 
     // add the newly created element and its content into the DOM 
-    document.getElementsByTagName("body")[0].appendChild(newDiv);;
-
+    document.getElementsByTagName("body")[0].appendChild(newDiv);
 
 }
+
 
 var loadTippy = function () {
     const template = document.querySelector('#template')
     const initialText = template.textContent
 
-    console.log('hi from tippy');
 
-    const tip = tippy('#criteriaDataBrowser', {
-        animation: 'shift-toward',
+
+    //console.log('hi from tippy');
+
+    const tip = tippy('#criteriaDataBrowser span span span', {
+        target: '',
+        interactive: true,
+        trigger: "click",
         arrow: true,
+        maxWidth: '40vh',
+        placement: 'left-start',
         html: '#template',
         onShow() {
             // `this` inside callbacks refers to the popper element
             const content = this.querySelector('.tippy-content')
 
+            console.log(this)
+
             if (tip.loading || content.innerHTML !== initialText) return
 
             tip.loading = true
 
-            var reader = new FileReader()
 
-            //fetch('https://ir.unlv.edu/UmbracoODS/umbraco/api/DataDictionaryApi/GetJson').then(resp => resp.json()).then(json => { console.log(json); })
+            fetch('https://ir.unlv.edu/UmbracoODS/umbraco/api/DataDictionaryApi/GetJson').then(resp => resp.json()).then(data => {
 
-            fetch('https://ir.unlv.edu/UmbracoODS/umbraco/api/DataDictionaryApi/GetJson').then(resp => resp.json()).then(blob => {
-                //const url = URL.createObjectURL(blob)
-                //console.log(JSON.parse(blob))
-                content.innerHTML = blob //'<img width="500" height="500" src="' + url + '">'
+
+                var dataParsed = JSON.parse(data)
+                var contentHtml = ""
+
+                //console.log(content)
+
+                for (var key in dataParsed) {
+                    if (dataParsed.hasOwnProperty(key)) {
+
+                        if (key == "Name") {
+                            //continue;
+                        }
+
+                        contentHtml += "<div class=\"p-0 m-0 mb-2\"><h4 class=\"mb-0 pb-0\" style=\"font-size:1rem;\">" + key + "</h4>" + dataParsed[key] + "</div>";
+                    }
+                }
+
+
+                content.innerHTML = contentHtml//'<img width="500" height="500" src="' + url + '">'
                 tip.loading = false
             }).catch(e => {
-                content.innerHTML = 'Loading failed'
+                content.innerHTML = 'Failed to load a definition'
                 tip.loading = false
             })
         },
@@ -88,8 +237,6 @@ var loadTippy = function () {
     })
 }
 
-
-
-
 addElement();
 setTimeout(loadTippy, 200);
+//loadTippy();
--- obiee-custom.js
+++ obiee-custom.js
@@ -2,41 +2,57 @@ var link = document.createElement("link");
 link.href = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css";
 link.type = "text/css";
 link.rel = "stylesheet";
+//document.getElementsByTagName("head")[0].appendChild(link);
+
+
+var link = document.createElement("link");
+link.href = "https://ir.unlv.edu/external-scripts/obiee-criteria-popover.css";
+link.type = "text/css";
+link.rel = "stylesheet";
 document.getElementsByTagName("head")[0].appendChild(link);
 
 var scripts = [
-    "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
-    "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js",
-    "https://ir.unlv.edu/external-scripts/obiee-criteria-popover.js"
-]
+     "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js",
+    // "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
+    // "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js",
+    "https://ir.unlv.edu/external-scripts/obiee-criteria-popover.js",
+    "https://unpkg.com/tippy.js@2.5.3/dist/tippy.all.min.js"
+];
 
 for (var i in scripts) {
     var script = document.createElement("script");
     script.type = "text/javascript";
     script.src = scripts[i];
-    document.getElementsByTagName("body")[0].appendChild(script);
+    document.getElementsByTagName("head")[0].appendChild(script);
 }
-
-
-
-
-
-
-
-
 
 function myFunc() {
     if (document.getElementById('criteriaDataBrowser')) {
-        
+
+        console.log(document.getElementById('criteriaDataBrowser'));
         console.log("the table should appear above");
-        
+
     } else {
         console.log('went to timeout');
         setTimeout(myFunc, 15);
-       
+
     }
 }
 
 myFunc();
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("criteriaDataBrowser").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "https://ir.unlv.edu/UmbracoODS/umbraco/api/DataDictionaryApi/GetJson", true);
+    xhttp.send();
+}
+
+
+//loadDoc();
 
